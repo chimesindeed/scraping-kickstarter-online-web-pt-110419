d7dc7add60c6b66296d8036c074f0be7983f3762
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-02-26 02:41:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,15 @@
 # require libraries/modules here
-
+require 'nokogiri'
+require 'pry'
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+  kickstarter = Nokogiri::HTML(html)
+  
 end
+# projects: kickstarter.css("li.project.grid4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# % funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+create_project_hash
